2300a48e4cf895bbb09eb7c54d7e71a260343533
Leonardo Prasetyo <leonardo.prasetyo5@gmail.com>
2024-09-18 00:45:12 +1000

    init image

--- components/LayoutHeader.vue
+++ components/LayoutHeader.vue
@@ -1,6 +1,10 @@
 <template>
     <div class="header__container">
-        <span>Leonardo Prasetyo</span>
+        <div class="header__logo">
+            <Image src="https://github.com/Beanmommo/leonardo-portfolio/blob/main/public/header__logo.png?raw=true"
+                alt="Logo" width="75" />
+            <span>Leonardo Prasetyo</span>
+        </div>
         <div>
             <span>Resume</span>
         </div>
@@ -11,6 +15,14 @@
 .header {
     &__container {
         display: grid;
+        grid-template-columns: 1fr 1fr 1fr;
+    }
+
+    &__logo {
+        display: flex;
+        align-items: center;
+        font-family: $logo-font;
+        font-size: 50px
     }
 }
 </style>
